21aad5581d7cbc9a790547fde431b35ce1fd9c19
SevaStopAll <Sevastopallrh@yandex.ru>
2023-12-10 18:59:00 +0400

    add Dockerfile

--- src/main/java/com/example/part1/Application.java
+++ src/main/java/com/example/part1/Application.java
@@ -9,5 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
     public class Application {
         public static void main(String[] args) {
             SpringApplication.run(Application.class);
+            System.out.println("Hello");
         }
     }
